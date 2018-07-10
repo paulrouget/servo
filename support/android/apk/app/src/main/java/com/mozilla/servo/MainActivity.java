15f5f1f27e624f73c19387710af937c19b31a00e
Paul Rouget <me@paulrouget.com>
2018-07-10 09:02:13 +0200

    remove lambda

--- support/android/apk/app/src/main/java/com/mozilla/servo/MainActivity.java
+++ support/android/apk/app/src/main/java/com/mozilla/servo/MainActivity.java
@@ -8,10 +8,8 @@ package com.mozilla.servo;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
