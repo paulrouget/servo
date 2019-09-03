05df3457516ac8df663cd0fb7dd587c60de77bb0
Paul Rouget <me@paulrouget.com>
2019-09-03 11:55:33 +0200

    WIP

---
+++ support/hololens/Servo/Servo.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include "Servo.h"
+#include "Servo.g.cpp"
+
+namespace winrt::Servo::implementation
+{
+    int32_t Servo::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void Servo::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+}
---
+++ support/hololens/Servo/Servo.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "Servo.g.h"
+
+namespace winrt::Servo::implementation
+{
+    struct Servo : ServoT<Servo>
+    {
+        Servo() = default;
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+    };
+}
+
+namespace winrt::Servo::factory_implementation
+{
+    struct Servo : ServoT<Servo, implementation::Servo>
+    {
+    };
+}
