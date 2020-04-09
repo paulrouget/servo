687481df0611b513b9cad560c6949e122fc277c8
Paul Rouget <me@paulrouget.com>
2020-04-09 09:55:38 +0200

    compiles

---
+++ support/hololens/ServoComponent/ServoControl.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include "ServoControl.h"
+#include "ServoControl.g.cpp"
+
+namespace winrt::ServoComponent::implementation
+{
+    int32_t ServoControl::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void ServoControl::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+}
---
+++ support/hololens/ServoComponent/ServoControl.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "ServoControl.g.h"
+
+namespace winrt::ServoComponent::implementation
+{
+    struct ServoControl : ServoControlT<ServoControl>
+    {
+        ServoControl() = default;
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+    };
+}
+
+namespace winrt::ServoComponent::factory_implementation
+{
+    struct ServoControl : ServoControlT<ServoControl, implementation::ServoControl>
+    {
+    };
+}
